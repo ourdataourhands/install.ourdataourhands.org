b734a0e03fb9b02318fb7475ec5a054e29150965
Ivan Stegic <ivan.stegic@gmail.com>
2017-03-03 21:12:25 -0600

    Add beacon script that logs via Zapier to Google Doc. Add blanks for onboot and oncron

---
+++ beacon.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Our Data Our Hands beacon
+#
+# A rudimentary attempt at identifying uniquely the storage pod's physical
+# hardware so we know how many nodes we have. If you find anything broken in
+# this script, please contact us and we will fix ASAP:
+#
+#	https://ourdataourhands.org/
+#	riseup@ourdataourhands.org
+#
+
+# ID file
+idfile="$HOME/.odohid"
+
+# Check for it
+if [[ ! -f "$idfile" ]]; then
+	uuidurl="https://www.uuidgenerator.net/api/version4"
+	odohid="$(curl -s $uuidurl | tr -d '\r')"
+	echo "$odohid" > $idfile
+else
+	odohid="$(cat $idfile)"
+fi
+
+# Timestamp
+ts="$(date +"%s")"
+
+# Public IP
+ip="$(curl -s http://icanhazip.com/)"
+
+# Hostname
+host="$(hostname -f)"
+
+# Event
+[ -z "$1" ] && event="ping" || event=$1
+
+# Ping!
+url="https://hooks.zapier.com/hooks/catch/2030249/mfl8sm/"
+zap="$(curl -s -G ${url} \
+	--data-urlencode publicip=${ip} \
+	--data-urlencode timestamp=${ts} \
+	--data-urlencode host=${host} \
+	--data-urlencode event=${event} \
+	--data-urlencode odohid=${odohid})"
---
+++ oncron-hourly.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+# Our Data Our Hands oncron hourly
+#
