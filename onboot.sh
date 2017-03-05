e56c49734d743d022fd462d57275ca78074055e4
Ivan Stegic <ivan.stegic@gmail.com>
2017-03-04 23:52:52 -0600

    Updates and start rise up

--- onboot.sh
+++ onboot.sh
@@ -2,4 +2,22 @@
 #
 # Our Data Our Hands onboot
 #
+
+# Phone home
 curl http://sh.ourdataourhands.org/beacon.sh | bash -s boot
+
+# Vars
+log="~/boot.log"
+riseup="/mnt/storage/docker/riseup.sh"
+
+# Updates
+apt-get update > $log
+apt-get dist-upgrade >> $log
+sudo apt-get clean >> $log
+
+# Rise up!
+if [[ -f "$riseup" ]]; then
+	bash $riseup
+else
+	echo "$riseup not found" >> $log
+fi
