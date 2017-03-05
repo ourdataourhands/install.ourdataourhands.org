de37d8235a2382f27593b2e7088f4b263e84249e
Ivan Stegic <ivan.stegic@gmail.com>
2017-03-05 10:51:06 -0600

    Add echo comments

--- onboot.sh
+++ onboot.sh
@@ -20,11 +20,13 @@ echo "$dt" > $log
 curl -s http://sh.ourdataourhands.org/beacon.sh | bash -s boot
 
 # Updates
+echo "Running updates..."
 sudo apt-get update >> $log
 sudo apt-get dist-upgrade >> $log
 sudo apt-get clean >> $log
 
 # Rise up!
+echo "Rising up..."
 if [[ -f "$riseup" ]]; then
 	echo "Run $riseup..." >> $log
 	sudo bash $riseup
