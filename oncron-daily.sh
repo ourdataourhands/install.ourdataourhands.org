efbd40eb018d3528c754c2eddfd2e53e7085b054
Ivan Stegic <ivan.stegic@gmail.com>
2017-03-13 14:30:10 -0500

    Add comments and a way to report bugs for the cron files.

--- oncron-daily.sh
+++ oncron-daily.sh
@@ -2,6 +2,11 @@
 #
 # Our Data Our Hands oncron-daily
 #
+# Runs using curl|bash on a daily basis.
+#
+# Report bugs here:
+# https://github.com/ourdataourhands/sh.ourdataourhands.org
+#
 
 # This is just a test
 curl -s http://sh.ourdataourhands.org/beacon.sh | bash -s crondaily
--- oncron-hourly.sh
+++ oncron-hourly.sh
@@ -2,6 +2,11 @@
 #
 # Our Data Our Hands oncron hourly
 #
+# Runs using curl|bash on a hourly basis.
+#
+# Report bugs here:
+# https://github.com/ourdataourhands/sh.ourdataourhands.org
+#
 
 # This is just a test
 curl -s http://sh.ourdataourhands.org/beacon.sh | bash -s cronhourly
