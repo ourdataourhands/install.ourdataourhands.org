af525f141b0ee87cdc407a88fd1e42513dc18142
Ivan Stegic <ivan.stegic@gmail.com>
2017-03-14 17:22:31 -0500

    Add lines to separate output a little better.

--- cleanup.sh
+++ cleanup.sh
@@ -15,25 +15,36 @@ if [ "$UID" != "0" ]; then
         fi
 fi
 
+echo "--------------------------------------------------"
 echo "Delete /home/pi/.odohid"
 $SUDO rm -f /home/pi/.odohid
+echo "--------------------------------------------------"
 echo "Delete logs"
 $SUDO rm -f /home/pi/*log
+echo "--------------------------------------------------"
 echo "Create /boot/firstboot"
 $SUDO touch /boot/firstboot
+echo "--------------------------------------------------"
 echo "Stop docker containers"
 $SUDO docker stop $($SUDO docker ps -a -q)
+echo "--------------------------------------------------"
 echo "Delete docker containers"
 $SUDO docker rm $($SUDO docker ps -a -q)
+echo "--------------------------------------------------"
 echo "Delete dangling docker images"
 $SUDO docker rmi $($SUDO docker images -f dangling=true -q)
+echo "--------------------------------------------------"
 echo "Delete docker images"
 $SUDO docker rmi $($SUDO docker images -a -q)
+echo "--------------------------------------------------"
 echo "Docker system prune"
 $SUDO docker system prune -f
+echo "--------------------------------------------------"
 echo "Reset hostname"
 $SUDO echo "storagepod" > /tmp/hostname
 $SUDO cp /tmp/hostname /etc/hostname
+echo "--------------------------------------------------"
 echo "Delete drive storage"
 $SUDO rm -fr /mnt/storage/.local
 $SUDO rm -fr /mnt/storage/*
+echo "--------------------------------------------------"
